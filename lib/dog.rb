f73181b6847b7a67c97ce11f0462053afd7b03b3
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-16 23:19:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,4 +40,12 @@ class Dog
     dog
   end
 
+  def self.new_from_db(row)
+  new_dog = self.new  # self.new is the same as running Song.new
+  new_dog.id = row[0]
+  new_dog.name =  row[1]
+  new_dog.breed = row[2]
+  new_dog  # return the newly created instance
+end
+
 end

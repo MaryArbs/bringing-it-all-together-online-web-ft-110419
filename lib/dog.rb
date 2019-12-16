76ae99967e8db37b3a101218c7ea16b9e4cab703
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-16 23:45:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,8 +60,8 @@ class Dog
     end.first
   end
 
-  def self.find_or_create_by(name:, album:)
-   song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+  def self.find_or_create_by(name:,  breed:)
+   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
    if !song.empty?
      song_data = song[0]
      song = Song.new(song_data[0], song_data[1], song_data[2])

92555ed13a5f0981be064fdaf709ec460c173730
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-16 23:09:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,6 +33,7 @@ class Dog
    SQL
    DB[:conn].execute(sql, self.name, self.breed)
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+   dog = Dog.new (self.name, self.breed)
   # end
  end
 end
